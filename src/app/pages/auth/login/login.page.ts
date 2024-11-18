1877a0356fcc4868695cc9272cd77aa242df7517
Andres <aramirez7310@uta.edu.ec>
2024-11-17 21:50:20 -0500

    graphical interface login completed

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements AfterViewInit {
 
+  animationCtrl = inject(AnimationController)
+
+  form= new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)])
+  })
+
+  async submit (){
+
+  }
   constructor() { }
 
-  ngOnInit() {
+  playAnimation() {
+    const animation = this.animationCtrl.create()
+    .addElement(document.querySelector('.logo')) // Seleccionar el elemento del DOM
+    .duration(3000) // Duración de la animación
+    .iterations(Infinity) // Repetir indefinidamente
+    .keyframes([
+      { offset: 0, transform: 'translateY(0)', easing: 'ease-in-out' }, // Posición inicial
+      { offset: 0.5, transform: 'translateY(-10px)', easing: 'ease-in-out' }, // Se eleva
+      { offset: 1, transform: 'translateY(0)', easing: 'ease-in-out' } // Regresa a la posición original
+    ]);
+  animation.play(); // Iniciar la animación
+  }
+
+  ngAfterViewInit(){
+    this.playAnimation();
   }
 
 }
