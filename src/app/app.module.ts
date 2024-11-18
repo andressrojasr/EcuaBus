dc968e33ee648e54357393dfb77eee0ed53e1658
Andres <aramirez7310@uta.edu.ec>
2024-11-18 14:04:05 -0500

    added signUp functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot({
     mode: "ios"
-  }), AppRoutingModule, SharedModule],
+  }), AppRoutingModule, SharedModule, AngularFireModule.initializeApp(environment.firebaseConfig)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export interface User{
+    uid:string;
+    name:string;
+    lastName: string;
+    card: string;
+    phone: string;
+    email:string;
+    password:string;
+    photo: string;
+    uidCooperative: string;
+    isBlocked: boolean;
+    rol:string;
+}
